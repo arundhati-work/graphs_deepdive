b7be8f7a2174f37fc828acf3e57296d9927f6969
Arundhati Bandopadhyaya <arundhatib.work@gmail.com>
2025-06-18 08:38:25 -0400

    added cycle check in hasPath

--- prob_hasPath.js
+++ prob_hasPath.js
@@ -31,3 +31,50 @@ const hasPath_bfs = (graph, src, dst) => {
     }
     return false;
 };
+
+
+// What if our graph  has a cycle? We have to account for that situation too
+
+const hasPath_withCycleCheck = (graph, src, dst) => {
+    const visited = new Set();
+    return hasPath_dfs_cycle(graph, src, dst, visited);
+}
+
+// DFS 
+const hasPath_dfs_cycle = (graph, src, dst, visited) => {
+    if (src === dst) {
+        return true;
+    }
+    visited.add(src);
+
+    for (let neighbor of graph[src]) {
+        if (!(visited.has(neighbor))){
+            if (hasPath_dfs_cycle(graph, neighbor, dst, visited) === true) {
+                return true;
+            }
+        }
+        
+    }
+    return false;
+};
+
+// We can also do this with BFS
+const hasPath_bfs_cycle = (graph, src, dst) => {
+    const queue = [src];
+    const visited = new Set();
+
+    while (queue.length > 0) {
+        const curr = queue.shift();
+        if (curr === dst) {
+            return true;
+        }
+        for (let neighbor of graph[curr]) {
+            if (!(visited.has(neighbor))){
+                visited.add(neighbor);
+                queue.push(neighbor);
+            }
+                
+        }
+    }
+    return false;
+};
